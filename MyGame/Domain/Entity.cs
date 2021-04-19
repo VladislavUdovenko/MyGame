8d697e0d2f7643f747544f4a87ad4ecb635ea202
VladislavUdovenko <vladislav.udovenko0@yandex.ru>
2021-04-19 23:56:01 +0500

    Initial commit

---
+++ MyGame/Domain/Entity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame.Domain
+{
+    public class Entity
+    {
+        //public int CurrentAnumation { get; set; }
+        //public int CurrentFrame { get; set; }
+
+        //public int IdleFrames { get; set; }
+        //public int RunFrames { get; set; }
+        //public int AtackFrames { get; set; }
+        //public int DeathFrames { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int DirectionX { get; set; }
+        public int DirectionY { get; set; }
+
+        public bool IsMoving { get; set; }
+
+        public Image CurrentSprite { get; set; }
+        // public int Size { get; set; }
+
+        public Entity(int x, int y, Image sprite)
+        {
+            X = x;
+            Y = y;
+            CurrentSprite = sprite;
+            //Size = 50;
+            //IdleFrames = idleFrames;
+            //RunFrames = runFrames;
+            //AtackFrames = atackFrames;
+            //DeathFrames = deathFrames;
+            //CurrentAnumation = 0;
+            //CurrentFrame = 0;
+        }
+
+        public void Move()
+        {
+            X += DirectionX;
+            Y += DirectionY;
+        }
+
+        //public void PlayAnimation(Graphics graphics)
+        //{
+        //    graphics.DrawImage(SpriteSheet, new Rectangle(new Point(X, Y), new Size(Size, Size)), 0, 0, Size, Size, GraphicsUnit.Pixel);
+        //    if (CurrentFrame < IdleFrames - 1)
+        //        CurrentFrame++;
+        //    else
+        //        CurrentFrame = 0;
+        //}
+    }
+}
---
+++ MyGame/Form1.cs
@@ -0,0 +1,90 @@
+﻿using MyGame.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    public partial class Form1 : Form
+    {
+        public Entity Player { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            Initialization();
+
+            DoubleBuffered = true;
+
+            var timer = new Timer();
+            var currentTick = 0;
+            timer.Interval = 50;
+            timer.Tick += (sender, args) =>
+            {
+                if (Player.IsMoving)
+                    Player.Move();
+                Invalidate();
+                currentTick = (currentTick + 1) % 10;
+            };
+            timer.Start();
+
+            Paint += (sender, args) =>
+            {
+                var graphics = args.Graphics;
+                graphics.DrawImage(Player.CurrentSprite, Player.X, Player.Y);
+            };
+
+            KeyDown += (sender, args) =>
+            {
+                var speed = 10;
+                Player.IsMoving = true;
+                switch (args.KeyCode)
+                {
+                    case Keys.W:
+                        Player.DirectionY = -speed;
+                        Player.CurrentSprite = currentTick < 5
+                        ? Resource1.DoomGuyGoingUpR
+                        : Resource1.DoomGuyGoingUpL;
+                        break;
+                    case Keys.S:
+                        Player.DirectionY = speed;
+                        Player.CurrentSprite = currentTick < 5
+                        ? Resource1.DoomGuyGoingDownR
+                        : Resource1.DoomGuyGoingDownL;
+                        break;
+                    case Keys.A:
+                        Player.DirectionX = -speed;
+                        Player.CurrentSprite = currentTick < 5
+                        ? Resource1.DoomGuyRunsToLeftR
+                        : Resource1.DoomGuyRunsToLeftL;
+                        break;
+                    case Keys.D:
+                        Player.DirectionX = speed;
+                        Player.CurrentSprite = currentTick < 5
+                        ? Resource1.DoomGuyRunsToRightR
+                        : Resource1.DoomGuyRunsToRightL;
+                        break;
+                }                
+            };
+
+            KeyUp += (sender, args) =>
+            {
+                Player.DirectionX = 0;
+                Player.DirectionY = 0;
+                Player.IsMoving = false;
+                Player.CurrentSprite = Resource1.DoomGuyStand;
+            };
+        }
+
+        public void Initialization()
+        {
+            Player = new Entity(730, 380, new Bitmap(Resource1.DoomGuyShootsDown));
+        }
+    }
+}
