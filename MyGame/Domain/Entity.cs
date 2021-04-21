a954f8e2c2a0a48cbdf81c565faaa171190c7bcc
VladislavUdovenko <vladislav.udovenko0@yandex.ru>
2021-04-21 21:08:00 +0500

    Update Entity.cs

--- MyGame/Domain/Entity.cs
+++ MyGame/Domain/Entity.cs
@@ -5,18 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyGame.Domain
+namespace MyGame
 {
     public class Entity
     {
-        //public int CurrentAnumation { get; set; }
-        //public int CurrentFrame { get; set; }
-
-        //public int IdleFrames { get; set; }
-        //public int RunFrames { get; set; }
-        //public int AtackFrames { get; set; }
-        //public int DeathFrames { get; set; }
-
         public int X { get; set; }
         public int Y { get; set; }
         public int DirectionX { get; set; }
@@ -25,20 +17,12 @@ namespace MyGame.Domain
         public bool IsMoving { get; set; }
 
         public Image CurrentSprite { get; set; }
-        // public int Size { get; set; }
 
         public Entity(int x, int y, Image sprite)
         {
             X = x;
             Y = y;
             CurrentSprite = sprite;
-            //Size = 50;
-            //IdleFrames = idleFrames;
-            //RunFrames = runFrames;
-            //AtackFrames = atackFrames;
-            //DeathFrames = deathFrames;
-            //CurrentAnumation = 0;
-            //CurrentFrame = 0;
         }
 
         public void Move()
@@ -46,14 +30,5 @@ namespace MyGame.Domain
             X += DirectionX;
             Y += DirectionY;
         }
-
-        //public void PlayAnimation(Graphics graphics)
-        //{
-        //    graphics.DrawImage(SpriteSheet, new Rectangle(new Point(X, Y), new Size(Size, Size)), 0, 0, Size, Size, GraphicsUnit.Pixel);
-        //    if (CurrentFrame < IdleFrames - 1)
-        //        CurrentFrame++;
-        //    else
-        //        CurrentFrame = 0;
-        //}
     }
 }
